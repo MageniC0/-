53b741f6e8439a85d97319fdab46817f9ae5a5a3
MageniC0 <abcdeffed123456654@163.com>
2025-03-16 07:22:33 +0000

    ,

--- 制作地图.py
+++ 制作地图.py
@@ -1,44 +1,55 @@
-import json, re
+import json
+import re
+
 help = """[help]
-load [path]: 编辑已有地图
-new [path]: 新建地图
+load [name]: 编辑已有地图
+new [name]: 新建地图
 cube [n]: 指定要放置的方块
-set [x] [y] [z]: 在(x, y, z)放置方块
+put [x] [y] [z]: 在指定位置放置方块
 done: 完成编辑
+output [name]: 另存文件
 """
 
-block = []
+class Block:
+    def __init__():
+        self.block = []
+        self.name = ""
+    
+    def load():
+        with open(self.name, "r") as f:
+            self.block = json.load(f)
+    
+    def new():
+        self.block = [[[0 for x in range(4)] for y in range(4)] for z in range(4)]
+    
+    def set_name(name):
+        self.name = name
+    
 
-def set(n, x, y, z):
-    block[z][y][x] = n
 
-def new_():
-    return [[[0 for x in range(4)] for y in range(4)] for z in range(4)]
 
-def load():
-    with open(name, "r") as f:
-        return json.load(f)
 
-def save():
-    with open(name, "w") as f:
-        json.dump(block, f)
 
-def done():
-    pass
+
+
+
+
 
 while True:
     ln = input("_")
     if ln == "":
         break
     elif me := re.match(r"load (\w+)", ln):
-        load(me.group(1))
+        Block.load(me.group(1))
     elif me := re.match(r"new (\w+)", ln):
-        new(me.group(1))
-    elif me := re.match(r"set (\d+) (\d+) (\d+)", ln):
-        set_(int(me.group(1)), int(me.group(2)), int(me.group(3)))
+        Block.new(me.group(1))
     elif me := re.match(r"cube (\d+)", ln):
         cube(int(group(1)), int(group(2)))
+    elif me := re.match(r"cube (\d+) (\d+) (\d+)", ln):
+        put(int(me.group(1)), int(me.group(2)), int(me.group(3)))
     elif ln == "done":
         done()
+    elif me := re.match(r"output (\w+)", ln):
+        Block.output(me.group(1))
     else:
         print(help)
--- 制作空地图.py
+++ 制作空地图.py
@@ -3,4 +3,4 @@ import json
 tr_blank = [[[0 for x in range(4)] for y in range(4)] for z in range(4)]
 
 with open("tr/blank.json", "w") as f:
-    json.dump(f)
+    json.dump(tr_blank, f)
