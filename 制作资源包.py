64d1013c0703fc5dd84b05518d8878790f01e4ab
MageniC0 <abcdeffed123456654@163.com>
2025-03-16 05:40:35 +0000

    .

--- 制作资源包.py
+++ 制作资源包.py
@@ -3,14 +3,14 @@ from PIL import Image
 import json
 
 n = 0
-image = Image.open("input.png")
-graph = [[[(0, 0, 0, 0) for i in range(13)] for j in range(13)] for n in range(256)]
-for y in range(16):
-    for x in range(16):
+image = Image.open("src/laer.png")
+graph = [[[(0, 0, 0, 0) for i in range(13)] for j in range(13)] for n in range(16)]
+for y in range(4):
+    for x in range(4):
         for j in range(13):
-            n = 16 * y + x
+            n = 4 * y + x
             for i in range(13):
                 graph[n][j][i] = image.getpixel((16 * y + j, 16 * x + i))
 
-with open("chc.json", "w") as f:
+with open("chc/laer.json", "w") as f:
     json.dump(graph, f)
