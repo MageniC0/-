85bf26f0b502d9f91b29e8fd44a45d36eb304450
MageniC0 <abcdeffed123456654@163.com>
2025-03-16 07:55:10 +0000

    .

--- 制作地图.py
+++ 制作地图.py
@@ -1,16 +1,6 @@
 import json
 import re
 
-help = """[help]
-load [name]: 编辑已有地图
-new [name]: 新建地图
-draw [n]: 指定要放置的方块
-point [x] [y] [z]: 在指定位置放置或删除方块
-clean: 删除模式或取消删除模式
-done: 完成编辑
-output [name]: 另存文件
-"""
-
 class Block:
     def __init__(self):
         self.block = []
@@ -25,10 +15,10 @@ class Block:
     
     def set_name(self, name):
         self.name = "tr/" + name
-
+    
     def done(self):
         print(f"已将地图保存至{self.name}")
-    
+
 class Mon:
     def __init__(self):
         self.draw_ = 0
@@ -54,24 +44,55 @@ class Mon:
 class Starlit:
     def __init__(self):
         self.ln = ""
+        self.help = """[help]
+        load [name]: 编辑已有地图
+        new [name]: 新建地图
+        draw [n]: 指定要放置的方块
+        point [x] [y] [z]: 在指定位置放置或删除方块
+        clean: 删除模式或取消删除模式
+        done: 完成编辑
+        output [name]: 另存文件
+        """
+
+    def load():
+        pass
+
+    def new():
+        pass
+
+    def draw():
+        pass
+
+    def point():
+        pass
+
+    def clean():
+        pass
+
+    def done():
+        pass
+
+    def output():
+        pass
+    
     def main(self):
         while True:
             self.ln = input("_")
             if self.ln == "":
                 break
-            elif me := re.match(r"load (\w+)", ln):
-                Mon.load(me.group(1))
-            elif me := re.match(r"new (\w+)", ln):
-                Mon.new(me.group(1))
-            elif me := re.match(r"draw (\d+)", ln):
-                Mon.draw(int(me.group(1)))
-            elif me := re.match(r"point (\d+) (\d+) (\d+)", ln):
-                Mon.point(int(me.group(1)), int(me.group(2)), int(me.group(3)))
-            elif ln == "clean": 
-                Mon.clean()
-            elif ln == "done":
-                Mon.done()
-            elif me := re.match(r"output (\w+)", ln):
-                Mon.output(me.group(1))
+            elif me := re.match(r"load (\w+)", self.ln):
+                self.load(me.group(1))
+            elif me := re.match(r"new (\w+)", self.ln):
+                self.new(me.group(1))
+            elif me := re.match(r"draw (\d+)", self.ln):
+                self.draw(int(me.group(1)))
+            elif me := re.match(r"point (\d+) (\d+) (\d+)", self.ln):
+                self.point(int(me.group(1)), int(me.group(2)), int(me.group(3)))
+            elif self.ln == "clean": 
+                self.clean()
+            elif self.ln == "done":
+                self.done()
+            elif me := re.match(r"output (\w+)", self.ln):
+                self.output(me.group(1))
             else:
                 print(help)
