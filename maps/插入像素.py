84432e7be23a53b5603216e6c4a0259665186ccf
MageniC0 <abcdeffed123456654@163.com>
2025-03-23 08:26:20 +0000

    o

---
+++ maps/插入像素.py
@@ -0,0 +1,4 @@
+pixels = [[[0, 0, 0, 0] for i in range(49)] for j in range(49)]
+
+pixels[0][1] = [1, 2, 3, 4]
+print(pixels[0])
---
+++ maps/调整后重新存储.py
@@ -0,0 +1,2 @@
+pixels = [[[0, 0, 0, 0] for i in range(49)] for j in range(49)]
+pixels[0][1] = [1, 2, 3, 4]
