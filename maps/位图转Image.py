8766ad9767711fa7ac813c9081082ddd23100c71
MageniC0 <abcdeffed123456654@163.com>
2025-03-23 07:58:51 +0000

    l

--- maps/位图转Image.py
+++ maps/位图转Image.py
@@ -2,4 +2,4 @@ from PIL import Image
 
 pixels = [[[0, 0, 0, 0] for i in range(49)] for j in range(49)]
 image = Image.frombytes('RGBA', (49, 49), bytes([channel for row in pixels for pixel in row for channel in pixel]))
-image.save("maps/test2.png")
+image.save("maps/test.png")
